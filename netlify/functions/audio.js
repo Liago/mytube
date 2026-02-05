e7a100a74982010bf3d07ff0706e003842486afe
Andrea <andrea@zampierolo.dev>
2026-02-05 14:52:31 +0100

    chore: removes verbose flag from youtube-dl options

--- netlify/functions/audio.js
+++ netlify/functions/audio.js
@@ -173,7 +173,6 @@ exports.handler = async (event, context) => {
 				'--referer', 'https://www.youtube.com/',
 				'--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 				'--write-info-json',
-				'--verbose',
 				'--js-runtimes', `node:${process.execPath}`
 			];
 
